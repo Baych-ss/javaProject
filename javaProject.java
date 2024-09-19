92d31bb4c578b0e114fead8e426f39516eab6451
Baych <baielkalmakov07@gmail.com>
2024-09-19 17:16:46 +0600

    while loop

--- javaProject.java
+++ javaProject.java
@@ -3,54 +3,62 @@ public class Main
 {
 	public static void main(String[] args) {
 	    Scanner scan = new Scanner(System.in);
+	    int[] array = new int[3];
 	    int firstBox = 145;
 	    int secondBox = 357;
 	    int thirdBox = 524;
-	    int[] array = new int[3];
 	    boolean isFisrtMatch;
 	    boolean isSecondMatch;
 	    boolean isThirdMatch;
 	    int count = 0;
+	    int allows = 4;
+	    while(allows > 0){
 		for(int i=0; i<3; i++){
 		    array[i] = scan.nextInt();
 		}
 		for(int i=0; i<3; i++){
 		    isFisrtMatch = array[0] == firstBox;
 		    if(isFisrtMatch){
-		        System.out.print("First box found");
-		        count++;
+		        System.out.println("First box found");
 		        break;
 		    }
 		    else 
-		        System.out.print("first box is not here");
+		        System.out.println("First box is not here");
 		    break;
     }
         for(int i=0; i<3; i++){
 		    isSecondMatch = array[1] == secondBox;
 		    if(isSecondMatch){
-		        System.out.print("First box found");
-		        count++;
+		        System.out.println("Second box found");
+		        
 		        break;
 		    }
 		    else 
-		        System.out.print("first box is not here");
+		        System.out.println("Second box is not here");
 		    break;
     }
         for(int i=0; i<3; i++){
 		    isThirdMatch = array[2] == thirdBox;
 		    if(isThirdMatch){
-		        System.out.print("First box found");
-		        count++;
+		        System.out.println("Third box found");
+		        
 		        break;
 		    }
 		    else 
-		        System.out.print("first box is not here");
+		        System.out.println("Third box is not here");
 		    break;
-    }
-        if(count == 3){
-            System.out.print("All boxes found");
-            System.exit(0);
+		    if ((isFisrtMatch) && (isSecondMatch) && (isThirdMatch)){
+                System.out.print("All boxes found");
+                System.exit(0);
         }
+        }
+		
+        
+        allows--;
+        System.out.println("You have " + allows + " aloows");
+	}
+	    System.out.println("Boxes changed their locations");
+	    System.exit(0);
         
 }
 }
