0858e973e217aa79dad7c584f86ab588de1e2349
Baych <baielkalmakov07@gmail.com>
2024-09-19 16:32:58 +0600

    added array

---
+++ javaProject.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class javaProject.index.java 
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    int kilometres = scan.nextInt();
+	    int firstBox = 145;
+	    int secondBox = 357;
+	    int thirdBox = 524;
+	    int[] array = new int[3];
+		for(int i=0; i<3; i++){
+		    array[i] = scan.nextInt();
+		}
+		
+    }
+    
+}
